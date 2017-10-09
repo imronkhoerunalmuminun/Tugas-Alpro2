b230131692e6d5f10f5a52a072cdbcc16e575ebc
imronkhoerunalmuminun <32305045+imronkhoerunalmuminun@users.noreply.github.com>
2017-10-09 10:31:54 -0700

    Create langkahmemasaknasi.cpp

---
+++ langkahmemasaknasi.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+cout << "==================================" << endl;
+cout << "|  Langkah-langkah Memasak Nasi  |" << endl;
+cout << "==================================" << endl;
+cout << "1. Ambil Wadah" << endl;
+cout << "2. Masukan Beras Kedalam Wadah" << endl;
+cout << "3. Masukan Air Lalu Cuci" << endl;
+cout << "4. Buang Air Lalu Masukan Kembali Air Sekuku" << endl;
+cout << "5. Setelah itu Masukan Kedalam Rice Cooker" << endl;
+cout << "6. Tekan Tombol Cook Pada Rice Cooker" << endl;
+cout << "7. Tunggu Hingga Lampu Indikator Berubah Menjadi Warm" << endl;
+cout << "8. Nasi Sudah Matang Dan Siap Dihidangkan" << endl;
+
+return 0;
+}
